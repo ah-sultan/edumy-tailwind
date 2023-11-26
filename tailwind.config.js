568d368f9a4f98ffccc894b8048ee40042550d4f
sultan-office <web.sultan54@gmail.com>
2023-11-26 17:33:15 +0600

    working on accordion section

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,10 @@ module.exports = {
       },
       colors : {
         "primary" : "#309255",
+        "primary-50" : "#F5FFF9",
+        "primary-100" : "#EEFBF3",
+        "primary-200" : "#D6FFE5",
+        "primary-300" : "#C5FFDC",
         "secondary": "#FFA200",
         "gray" : "#747474",
         "lightGray" : "#56616F",
@@ -32,6 +36,43 @@ module.exports = {
       fontFamily : {
         "urbanist" : `'Urbanist', sans-serif`,
         "hindSiliguri" : `'Hind Siliguri', sans-serif`
+      },
+      lineHeight : {
+        "22px" : "22px",
+        "30px" : "30px",
+        "48px" : "48px"
+      },
+      fontSize : {
+        "22px" : "22px",
+        "34px" : "34px"
+      }, 
+      spacing: {
+        "5px" : "5px",
+        "7px" : "7px",
+        "13px" : "13px",
+        "15px" : "15px",
+        "18px" : "18px",
+        "19px" : "19px",
+        "21px" : "21px",
+        "25px" : "25px",
+        "30px" : "30px",
+        "38px" : "38px",
+        "45px" : "45px",
+        "50px" : "50px",
+        "55px" : "55px",
+        "65px" : "65px",
+        "90px" : "90px",
+        "70px" : "70px",
+        "88px" : "88px",
+        "100px" : "100px",
+     
+      },
+      zIndex : {
+        1 : 1
+      },
+      borderRadius : {
+        "10px" : "10px",
+        "15px" : "15px"
       }
     },
   },
